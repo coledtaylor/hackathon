aadc0551e7bd17132932ef1b88be63e57aa03892
terryalexis <70789551+terryalexis@users.noreply.github.com>
2022-03-19 07:06:49 -0600

    Create gameState.lua

---
+++ src/gameState.lua
@@ -0,0 +1,14 @@
+-- Game States:
+-- 0 = Before start
+-- 1 = Game
+-- 2 = Game Over
+
+
+function updateState(dt)
+    if(gameState == 0)
+        --do something
+    elseif(gameState == 1)
+        --do something else
+    elseif(gameState == 2)
+        --end game/level
+end
