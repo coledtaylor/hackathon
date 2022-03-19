0d3cce1c0b50e1ecb0dbb2922f361c99daebbd08
lrfalslev <lrfalslev@gmail.com>
2022-03-19 03:16:23 -0600

    add map

---
+++ assets/Maps/7DRL-Tiles2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="7DRL-Tiles2" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="hackathon/assets/Zombie Asset Pack/7DRL-Tiles2.png" width="256" height="256"/>
+</tileset>
